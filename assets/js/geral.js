c8f31356ee8d31b4bd6dce9e2f27ddf74afc1761
FabianaRossatto <fmrossatto@senacrs.com.br>
2025-03-26 11:48:47 -0300

    conteudo

--- assets/js/geral.js
+++ assets/js/geral.js
@@ -1,20 +1,51 @@
-window.addEventListener('load', function(){ 
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
+window.addEventListener('load', function () {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
 
-    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-      return new bootstrap.Popover(popoverTriggerEl)
-    })
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
 
-    $('.preloader').fadeOut(300);
+  $('.preloader').fadeOut(300);
 
-    $('.botao').click(function(){
-      $(this).addClass('visitado');
-    });
+  $('.botao').click(function () {
+    $(this).addClass('visitado');
+  });
 })
+
+// botao com animacao
+window.addEventListener('load', function () {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+
+  $('.preloader').fadeOut(300);
+
+  $('.botaoCheck').click(function () {
+    $(this).addClass('visitado2');
+  });
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 // reposicionamento do botao do menu
 
@@ -27,8 +58,8 @@ $(window).scroll(function () {
     $('.bt-hamburguer').css("top", "20px");
     $('.bt-fecharmenu').css("top", "20px");
   } else {
-    $('.bt-hamburguer').css("top", "150px");      
-    $('.bt-fecharmenu').css("top", "150px");      
+    $('.bt-hamburguer').css("top", "150px");
+    $('.bt-fecharmenu').css("top", "150px");
   }
 
 }); // scroll END
@@ -48,46 +79,46 @@ $(window).scroll(function () {
   } else {
     $('body').removeClass("top00");
     $('body').addClass("top60");
-    $('.bt-hamburguer').css("top", "150px");      
-    $('.bt-fecharmenu').css("top", "150px");      
+    $('.bt-hamburguer').css("top", "150px");
+    $('.bt-fecharmenu').css("top", "150px");
   }
 
 });
 var isDarkMode = true; // Controla o modo atual (escuro/claro)
 
 function toggleBackgroundImage() {
-    var button = document.getElementById('toggleButton');
-    isDarkMode = !isDarkMode; // Alternar entre escuro e claro
+  var button = document.getElementById('toggleButton');
+  isDarkMode = !isDarkMode; // Alternar entre escuro e claro
 
-    if (isDarkMode) {
-        button.style.backgroundImage = "url('../assets/images/modo_escuro.svg')";
-    } else {
-        button.style.backgroundImage = "url('../assets/images/modo_claro.svg')";
-    }
+  if (isDarkMode) {
+    button.style.backgroundImage = "url('../assets/images/modo_escuro.svg')";
+  } else {
+    button.style.backgroundImage = "url('../assets/images/modo_claro.svg')";
+  }
 }
 
 $(".btn-tema_pagina").click(function () {
   console.log($(".btn-tema_pagina"))
   if ($("body").hasClass('bg-claro')) {
-      $("body").removeClass('bg-claro')
-      $("body").addClass("bg-escuro");
+    $("body").removeClass('bg-claro')
+    $("body").addClass("bg-escuro");
   } else if ($("body").hasClass('bg-escuro')) {
-      $("body").removeClass('bg-escuro')
-      $("body").addClass("bg-claro")
+    $("body").removeClass('bg-escuro')
+    $("body").addClass("bg-claro")
   }
 });
 
 // MODO ESCURO
 
 // caixas expansiveis
-$('.caixa-container button.btpluscaixa').click(function(){
+$('.caixa-container button.btpluscaixa').click(function () {
   let caixa = $(this).prev()
-  if(caixa.hasClass('aberta')){
-      caixa.removeClass('aberta')
-      $(this).css('background-image', 'url("../assets/images/bt_mais.svg")')
+  if (caixa.hasClass('aberta')) {
+    caixa.removeClass('aberta')
+    $(this).css('background-image', 'url("../assets/images/bt_mais.svg")')
   } else {
-      caixa.addClass('aberta')
-      $(this).css('background-image', 'url("../assets/images/bt_menos.svg")')
+    caixa.addClass('aberta')
+    $(this).css('background-image', 'url("../assets/images/bt_menos.svg")')
   }
 })
 
